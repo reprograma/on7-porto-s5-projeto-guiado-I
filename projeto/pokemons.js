26db7acf850074cb73c1fc004d629b5141f6f0ce
AnnaNee <annaflaviac@me.com>
2020-09-05 13:00:05 -0300

    Adiciona arquivos do projeto

---
+++ projeto/pokemons.js
@@ -0,0 +1,33 @@
+const db = require('./database')
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.table(db.pokemons)
+
+rl.question('O que você gostaria de fazer com seus pokemons? \n 1. Registrar \n 2. Treinar \n', function(instrucao) {
+  if (instrucao == 'registrar') {
+    rl.question('Qual o nome do pokemon? \n', function(nomePokemon) {
+      rl.question('Qual o nível do pokemon? \n', function(nivelPokemon) {
+        rl.question('Qual o tipo do pokemon? \n', function(tipoPokemon) {
+          console.log('Nome:', nomePokemon, 'Nível:', nivelPokemon, 'Tipo:', tipoPokemon)
+          // Você pode remover esse console.log acima se quiser.
+          // Chame AQUI a função que irá receber os valores e registrar o pokemon.
+          rl.close()
+        });
+      });
+    });
+  } else {
+    rl.question('Qual o ID do pokemon? \n', function(idPokemon) {
+      rl.question('Quantos níveis quer adicionar? \n', function(niveisPokemon) {
+        console.log('ID:', idPokemon, 'Níveis:', niveisPokemon)
+        // Você pode remover esse console.log acima se quiser.
+        // Chame AQUI a função que irá receber os valores e treinar o pokemon.
+        rl.close()
+      });
+    });
+  }
+  console.log(instrucao)
+});
