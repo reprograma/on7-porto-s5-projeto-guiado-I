64778c3ad504a754ec5ed5d734d4de6bdbbeff57
lealclarissa <lealclarissa2@gmail.com>
2020-09-07 00:07:16 -0300

    Exercício 7 - resolvido

--- exercicios/exercicio7.js
+++ exercicios/exercicio7.js
@@ -1,1 +1,13 @@
+//7. Some somente os valores ímpares de uma lista
+
 const valores = [2, 34, 56, 3, 9, 6, 55, 106];
+
+// 1ª etapa: criar nova array somente com números ímpares:
+
+const numerosImpar = valores.filter(item => item % 2 !== 0) 
+//console.log(somaImpar) //// [3, 9, 55]
+
+// 2ª etapa: somar os valores da nova array:
+
+const somaImpar = numerosImpar.reduce((acumulador, itemAtual) => acumulador + itemAtual)
+console.log(`A soma de todos os valores ímpares é: ${somaImpar}`)
