0bc16d712d008296a93a22f1be02f11a09e2c32d
mflilian <mayhhara@hotmail.com.br>
2020-09-11 10:31:16 -0300

    Resolvido

--- exercicios/exercicio7.js
+++ exercicios/exercicio7.js
@@ -1,1 +1,7 @@
-const valores = [2, 34, 56, 3, 9, 6, 55, 106];
+//Some somente os valores ímpares de uma lista
+
+ const valores = [2, 34, 56, 3, 9, 6, 55, 106];
+
+// array [2, 34, 56, 3, 9, 6, 55, 106]
+
+console.log((valores.filter(item => item % 2 !== 0)).reduce((acc, item) => acc + item))
