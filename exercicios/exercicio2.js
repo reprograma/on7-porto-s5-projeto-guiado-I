31ced99adce9c50033181aec0c6f3101dcc885d4
mflilian <mayhhara@hotmail.com.br>
2020-09-11 10:25:28 -0300

    Resolvido

---
+++ exercicios/exercicio2.js
@@ -0,0 +1,14 @@
+// Liste a tabuada do 6 (do 1 ao 10)
+
+let number = 6;
+while (number <= 60) {
+    console.log(number);
+    number = number + 6 ;
+}
+
+
+// ou 
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i*6)
+}
