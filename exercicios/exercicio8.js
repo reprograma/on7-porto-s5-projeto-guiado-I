3b5f9748fda74d5914cd9f49056fd893d2c87bb4
mflilian <mayhhara@hotmail.com.br>
2020-09-11 10:32:03 -0300

    Resolvido

--- exercicios/exercicio8.js
+++ exercicios/exercicio8.js
@@ -1,1 +1,6 @@
+// 8. Filtre uma lista de nomes e retorne somente os que começam com a letra 'A'
+
+
 const nomes = ['Anna', 'Andressa', 'Luciana', 'Bruna', 'Amanda'];
+
+console.log(nomes.filter(nomes => nomes.substr(0,1,4) == 'A')) 
