abf5bbaea4b478942f8078053b0869a3dfe5ef84
mflilian <mayhhara@hotmail.com.br>
2020-09-11 10:33:36 -0300

    Resolvido

--- exercicios/exercicio9.js
+++ exercicios/exercicio9.js
@@ -1,7 +1,13 @@
+// Desafio surpresa (:
+
 const bala = ['[<O>]'];
 const pessoa = {
-  nome: 'Anna',
-  comer(bala) {
+  nome: 'Carlos',
+  comer: function(bala) {
+    
     console.log(`${this.nome} comeu a bala: ${bala}`);
   }
 }
+
+conbala[0] = "O"
+pessoa.comer(bala)
