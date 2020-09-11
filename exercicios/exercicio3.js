ee6b3f7bdbc54f73d5d4ecb92f954ddc8d1322a5
mariana.vns <mariana.vns@gmail.com>
2020-09-11 15:24:14 -0300

    Resolve os exercícios 2 e 3

--- exercicios/exercicio3.js
+++ exercicios/exercicio3.js
@@ -1,1 +1,7 @@
+// 3. Liste uma lista de mercado
+
 const listaDeMercado = ['maçã', 'banana', 'chamyto', 'nutella', 'coca-cola', 'bisnaguinha', 'rap10']
+
+const constroiLista = () => {for (i = 0; i < listaDeMercado.length; i++){console.log(`item ${i+1}: ${listaDeMercado[i]}`)}}
+
+constroiLista()
