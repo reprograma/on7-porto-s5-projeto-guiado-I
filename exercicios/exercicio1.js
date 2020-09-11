9040ad68747d2c63af447c70cb9da42e4dfaaef7
mariana.vns <mariana.vns@gmail.com>
2020-09-11 13:48:59 -0300

    Respondido exercício 1

---
+++ exercicios/exercicio1.js
@@ -0,0 +1,22 @@
+
+// 1. Liste números de 0 a 10 no console
+
+function zeroADez (){
+for (let i = 0; i <= 10; i++){
+        console.log(i)
+    }
+}
+
+zeroADez()
+
+// Arrow Function
+
+const zeroADez = () => { for (let i = 0; i <= 10; i++) {
+    console.log(i)
+    }
+}
+
+zeroADez()
+
+
+
