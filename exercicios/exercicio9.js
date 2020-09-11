a19411041ee2649f97c0a7e847a788fadccc27bf
romanellipereira <romanelli.pereira@gmail.com>
2020-09-11 00:04:49 -0300

    atualizando exercicio 9 - resolução de conflito

--- exercicios/exercicio9.js
+++ exercicios/exercicio9.js
@@ -1,7 +1,9 @@
 const confeito = ['>(O)<'];
+confeito[0] = '0'
 const pessoa = {
   nome: 'Anna',
   comer(bala) {
     console.log(`${this.nome} comeu o confeito: ${bala}`);
   }
 }
+pessoa.comer(confeito)
