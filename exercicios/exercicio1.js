160eb7c313187cccc12e2086626fb88da830ffc1
mflilian <mayhhara@hotmail.com.br>
2020-09-11 10:23:29 -0300

    Resolvido

---
+++ exercicios/exercicio1.js
@@ -0,0 +1,13 @@
+// Liste números de 0 a 10 no console
+
+  let numero = 0;
+  while (numero <= 10) {
+     console.log(numero);
+     numero = numero + 1 ;
+  }
+
+ //ou 
+ 
+  for (let i = 0; i <= 10 ; i++) {
+    console.log(i)
+  }
