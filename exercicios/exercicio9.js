ce7a86b39e13d9d47991d15f22a8c8a91b5ef9a0
AnnaNee <annaflaviac@me.com>
2020-09-10 22:19:11 -0300

    Adiciona exercício do jogo e atualiza conteúdos da aula

--- exercicios/exercicio9.js
+++ exercicios/exercicio9.js
@@ -1,7 +1,7 @@
-const bala = ['[<O>]'];
+const confeito = ['>(O)<'];
 const pessoa = {
-  nome: 'Anna',
-  comer(bala) {
-    console.log(`${this.nome} comeu a bala: ${bala}`);
+  nome: 'Carlos',
+  comer(confeito) {
+    console.log(`${this.nome} comeu o confeito: ${praSuavariavel}`);
   }
 }
