c3624c4ee07fc284cf117672c2cac5064d846895
diaslilian <lilian.dias25@gmail.com>
2020-09-11 10:11:04 -0300

    resolução do projeto em andamento

--- projeto/pokemons.js
+++ projeto/pokemons.js
@@ -1,33 +1,69 @@
-const db = require('./database')
-const readline = require('readline');
+const db = require("./database");
+const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-console.table(db.pokemons)
+console.table(db.pokemons);
 
-rl.question('O que você gostaria de fazer com seus pokemons? \n 1. Registrar \n 2. Treinar \n', function(instrucao) {
-  if (instrucao == 'registrar') {
-    rl.question('Qual o nome do pokemon? \n', function(nomePokemon) {
-      rl.question('Qual o nível do pokemon? \n', function(nivelPokemon) {
-        rl.question('Qual o tipo do pokemon? \n', function(tipoPokemon) {
-          console.log('Nome:', nomePokemon, 'Nível:', nivelPokemon, 'Tipo:', tipoPokemon)
-          // Você pode remover esse console.log acima se quiser.
-          // Chame AQUI a função que irá receber os valores e registrar o pokemon.
-          rl.close()
+rl.question(
+  "O que você gostaria de fazer com seus pokemons? \n 1. Registrar \n 2. Treinar \n",
+  function (instrucao) {
+    if (
+      instrucao == "registrar" ||
+      instrucao == "Registrar" ||
+      instrucao == 1
+    ) {
+      rl.question("Qual o nome do pokemon? \n", function (nomePokemon) {
+        rl.question("Qual o nível do pokemon? \n", function (nivelPokemon) {
+          rl.question("Qual o tipo do pokemon? \n", function (tipoPokemon) {
+            // console.log(
+            //   "Nome:",
+            //   nomePokemon,
+            //   "Nível:",
+            //   nivelPokemon,
+            //   "Tipo:",
+            //   tipoPokemon
+            // );
+            // Você pode remover esse console.log acima se quiser.
+            // Chame AQUI a função que irá receber os valores e registrar o pokemon.
+
+            const registrarPokemon = (nome, nivel, tipo) => {
+              const pokemon = {
+                id: db.pokemons.length + 1,
+                nome,
+                nivel: parseInt(nivel),
+                tipo: tipo.split(","),
+              };
+
+              return db.pokemons.push(pokemon);
+            };
+
+            registrarPokemon(nomePokemon, nivelPokemon, tipoPokemon);
+            console.table(db.pokemons);
+
+            rl.close();
+          });
         });
       });
-    });
-  } else {
-    rl.question('Qual o ID do pokemon? \n', function(idPokemon) {
-      rl.question('Quantos níveis quer adicionar? \n', function(niveisPokemon) {
-        console.log('ID:', idPokemon, 'Níveis:', niveisPokemon)
-        // Você pode remover esse console.log acima se quiser.
-        // Chame AQUI a função que irá receber os valores e treinar o pokemon.
-        rl.close()
+    } else {
+      rl.question("Qual o ID do pokemon? \n", function (idPokemon) {
+        rl.question("Quantos níveis quer adicionar? \n", function (
+          niveisPokemon
+        ) {
+          console.log("ID:", idPokemon, "Níveis:", niveisPokemon);
+          // Você pode remover esse console.log acima se quiser.
+          // Chame AQUI a função que irá receber os valores e treinar o pokemon.
+
+          const treinarPokemons = (idPokemon) => {};
+
+          treinarPokemons();
+
+          rl.close();
+        });
       });
-    });
+    }
+    console.log(instrucao);
   }
-  console.log(instrucao)
-});
+);
