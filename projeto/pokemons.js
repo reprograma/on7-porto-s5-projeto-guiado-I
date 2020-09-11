b9d71825c6c450d9188314f7a988abd8d3c1b857
mariana.vns <mariana.vns@gmail.com>
2020-09-11 10:50:00 -0300

    Localiza o poke a treinar e substitui seu nível pelo adicionado pelo usuário (ainda nao adiciona com o nível anterior)Localiza o poke a treinar e substitui seu nível pelo adicionado pelo usuário (ainda nao adiciona com o nível anterior)

--- projeto/pokemons.js
+++ projeto/pokemons.js
@@ -8,47 +8,54 @@ const rl = readline.createInterface({
 
 console.table(db.pokemons)
 
-rl.question('O que você gostaria de fazer com seus pokemons? \n 1. Registrar \n 2. Treinar \n', function(instrucao) {
-  if (instrucao == 'Registrar') {
-    rl.question('Qual o nome do pokemon? \n', function(nomePokemon) {
-      rl.question('Qual o nível do pokemon? \n', function(nivelPokemon) {
-        rl.question('Qual o tipo do pokemon? \n', function(tipoPokemon) {
-          console.log('Nome:', nomePokemon, 'Nível:', nivelPokemon, 'Tipo:', tipoPokemon)
-          //  Chame AQUI a função que irá receber os valores e registrar o pokemon.
-          function registerPoke (pokeName, pokeLevel, pokeType){
+rl.question('O que você gostaria de fazer com seus pokemons? \n 1. Registrar \n 2. Treinar \n', function(instruction) {
+  if (instruction == 'Registrar') {
+    rl.question('Qual o nome do pokemon? \n', function(namePokemon) {
+      rl.question('Qual o nível do pokemon? \n', function(levelPokemon) {
+        rl.question('Qual o tipo do pokemon? \n', function(typePokemon) {
+          console.log('Nome:', namePokemon, 'Nível:', levelPokemon, 'Tipo:', typePokemon)
+          
+          function registerPoke (pokeId, pokeName, pokeLevel, pokeType){
           let registeredPoke = {
-              id: parseInt(db.pokemons.length + 1), 
+              id: parseInt(pokeId), 
               nome: pokeName,
               nivel: parseInt(pokeLevel),
               tipo: [pokeType],
           }
       return registeredPoke
 }
-db.pokemons.push(registerPoke(nomePokemon, nivelPokemon, tipoPokemon))
+db.pokemons.push(registerPoke(parseInt(db.pokemons.length + 1), namePokemon, levelPokemon, typePokemon))
+
 console.table(db.pokemons)
 
           rl.close()
         });
       });
     });
-  } else if (instrucao == 'Treinar') {
+  } else if (instruction == 'Treinar') {
     rl.question('Qual o ID do pokemon? \n', function(idPokemon) {
-      rl.question('Quantos níveis quer adicionar? \n', function(niveisPokemon) {
-        console.log('ID:', idPokemon, 'Níveis:', niveisPokemon)
+      rl.question('Quantos níveis quer adicionar? \n', function(levelUpPokemon) {
+        console.log('ID:', idPokemon, 'Níveis:', levelUpPokemon)
         // Você pode remover esse console.log acima se quiser.
                  
-          if (idPokemon > db.pokemons.length){
-          console.log('Pokemon não encontrado')
-         } else {
-          console.log('Pokemon Encontrado')
-         }
+        function treinaPoke (pokeId, pokeName, pokeLevel, pokeType){
+          let pokemonTreinado = {
+              id: parseInt(pokeId),
+              nome: pokeName,
+              nivel: parseInt(pokeLevel),
+              tipo: pokeType
+              }
+      return pokemonTreinado
+}
+db.pokemons[parseInt(idPokemon) - 1] = treinaPoke(parseInt(idPokemon), "TESTE-NOME", levelUpPokemon, "TESTE-TIPO")
+console.table(db.pokemons)
        
         rl.close()
       });
     });
-  } else if (instrucao != 'Treinar' && instrucao != 'Registrar'){
+  } else if (instruction != 'Treinar' && instruction != 'Registrar'){
     console.log('ERRO: Só são aceitas as instruções "Treinar" ou "Registrar"')
     rl.close()
     }
-  // console.log(instrucao)
+  // console.log(instruction)
 });
