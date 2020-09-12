5cfea482fc6655589826a386aae27c6570120c37
jessica-MBS <jessica.aparecida@gmail.com>
2020-09-12 19:46:21 -0300

    Exercicios de 1 até 7 concluído

--- exercicios/exercicio3.js
+++ exercicios/exercicio3.js
@@ -1,1 +1,11 @@
+//3. Liste uma lista de mercado 
+
 const listaDeMercado = ['maçã', 'banana', 'chamyto', 'nutella', 'coca-cola', 'bisnaguinha', 'rap10']
+
+function listaMercado () {
+for(let i = 0; i < listaDeMercado.length; i++) {
+        console.log(listaDeMercado[i])    
+}
+}
+
+listaMercado()
--- exercicios/exercicio7.js
+++ exercicios/exercicio7.js
@@ -1,1 +1,16 @@
+//7. Some somente os valores ímpares de uma lista
+
 const valores = [2, 34, 56, 3, 9, 6, 55, 106];
+
+const arrayImpar = []
+  for(let i=0; i<valores.length; i++){
+  if (valores[i] % 2 != 0) {
+    arrayImpar.push(valores[i])
+  }
+}
+//console.log(arrayImpar)
+
+let soma = arrayImpar.reduce(function(soma, numero){
+    return soma + numero}, 0)
+console.log(soma)
+
