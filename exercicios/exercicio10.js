bfd895aca6cf9ea4811b081e1345f0a978526a50
Audry-prog <audryavalos@hotmail.com>
2020-09-12 14:59:31 -0300

    Instalei o readline sync

--- exercicios/exercicio10.js
+++ exercicios/exercicio10.js
@@ -5,25 +5,20 @@ Vamos criar um jogo que adivinha um número! O jogo irá gerar um número randô
 Com o número secreto gerado, o jogo irá perguntar ao jogador: Que número estou pensando? Se o jogador acertar, então a máquina diz: Parabéns, você acertou! Mas se o jogador errar, a máquina precisa dizer se o número digitado é maior ou menor que o número secreto.
 
 Vamos lá?*/
+const input = require('readline-sync');
 
-const readline = require('readline');
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
-});
-
-const numSecreto = Math.floor(Math.random() * 100);;
-console.log ('Estou pensando num número secreto entre 0 e 100. Tente advinhar!') 
-const perguntarNumero = function () {
-	rl.question('Qual o número secreto? \n', function (resposta) { 
-        // Aqui você coloca a lógica para verificar se a resposta é correta ou não.
-        if (resposta === numSecreto) {
-    console.log ('Parabéns você acertou o número!')
-        } else if (resposta < numSecreto) {
-    console.log ('Errado! Seu palpite está muito baixo.Tente outra vez: ');
-        } else {
-    console.log('O número que você digitou é maior que o número secreto. Tente outra vez:');
-        }
-    })
-}; 
-perguntarNumero();
+const numSecreto = Math.floor(Math.random() * 50);
+console.log ('Estou pensando num número secreto entre 0 e 50. Tente advinhar!') 
+let resposta = 0
+    
+do {
+	let resposta = input.questionInt('Qual o número secreto?');
+	if (resposta < numSecreto) {
+		console.log('Errado! Seu palpite está muito baixo.Tente outra vez: ');
+	} else if (resposta > numSecreto) {
+		console.log('Errado! Seu palpite está muito alto.Tente outra vez: ');
+	} else if (resposta === numSecreto) {
+		console.log('Parabéns você acertou o número!');
+		break;
+	}
+} while (resposta != numSecreto);
