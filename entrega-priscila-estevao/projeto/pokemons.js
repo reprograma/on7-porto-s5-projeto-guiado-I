57e6309abd4d85a84384e74934de651ca185d453
Priscila Estevao da Cunha <priscila.cunha@demprel.local>
2020-09-12 20:18:49 -0300

    trocando as váriáveis por nomes em inglês

--- entrega-priscila-estevao/projeto/pokemons.js
+++ entrega-priscila-estevao/projeto/pokemons.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-console.table(db.pokemons)
+console.table(db.pokemons);
 
 let menu = () => {
   rl.question('O que você gostaria de fazer com seus pokemons? \n 1. Registrar \n 2. Treinar \n', function(instruction) {
@@ -15,31 +15,31 @@ let menu = () => {
       rl.question('Qual o nome do pokemon? \n', function(namePokemon) {
         rl.question('Qual o nível do pokemon? \n', function(levelPokemon) {
           rl.question('Qual o tipo do pokemon? \n', function(typePokemon) {
-            const registerPokemon = (nome, nivel, tipo) => {
+            const registerPokemon = (name, level, type) => {
               let newPokemon = {
                 id: parseInt(db.pokemons.length + 1),
-                nome: nome,
-                nivel: parseInt(nivel),
-                tipo: tipo.split(',')
+                nome: name,
+                nivel: parseInt(level),
+                tipo: type.split(',')
               } 
               return newPokemon
             }
             db.pokemons.push(registerPokemon(namePokemon, levelPokemon, typePokemon))
             console.table(db.pokemons)
             rl.close()
-          })
-        })
-      })
+          });
+        });
+      });
     } else if (instruction == 'treinar' || instruction == 'Treinar' || instruction == 2) {
-        let question = () => {
+        let idQuestion = () => {
           rl.question('Qual o ID do pokemon? \n', function(idPokemon) {
             if(!db.pokemons[idPokemon - 1]) {
               console.log('Sorry! Pokemon não encontrado. Digite uma id válida.')
-              question()
+              idQuestion()
             }
             rl.question('Quantos níveis quer adicionar? \n', function(levelsPokemon) {
-              const trainPokemon = (id, niveis) => {
-                let newLevel = db.pokemons[id - 1].nivel + parseInt(niveis)
+              const trainPokemon = (id, levels) => {
+                let newLevel = db.pokemons[id - 1].nivel + parseInt(levels)
                 if(newLevel > 100) {
                   db.pokemons[id - 1].nivel = 100
                 } else {
@@ -49,14 +49,14 @@ let menu = () => {
               trainPokemon(idPokemon, levelsPokemon)
               console.table(db.pokemons)
               rl.close()
-            })
-          })
-        }
-        question()
+            });
+          });
+        };
+        idQuestion()
     } else {
       console.log('Comando inválido. Escolha uma das opções: 1. Registrar ou 2. Treinar.')
       menu()
     }
-  })
-}
-menu()
+  });
+};
+menu();
