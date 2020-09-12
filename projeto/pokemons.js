8f5fcae560ad1ce0bda207120b7656760b830ea8
jessica-MBS <jessica.aparecida@gmail.com>
2020-09-12 20:29:48 -0300

    projeto - concluido registro pokemon

--- projeto/pokemons.js
+++ projeto/pokemons.js
@@ -12,9 +12,19 @@ rl.question('O que você gostaria de fazer com seus pokemons? \n 1. Registrar \n
     rl.question('Qual o nome do pokemon? \n', function(nomePokemon) {
       rl.question('Qual o nível do pokemon? \n', function(nivelPokemon) {
         rl.question('Qual o tipo do pokemon? \n', function(tipoPokemon) {
-          console.log('Nome:', nomePokemon, 'Nível:', nivelPokemon, 'Tipo:', tipoPokemon)
-          // Você pode remover esse console.log acima se quiser.
-          // Chame AQUI a função que irá receber os valores e registrar o pokemon.
+          // console.log('Nome:', nomePokemon, 'Nível:', nivelPokemon, 'Tipo:', tipoPokemon)
+          
+          function registroPokemon(nome,nivel,tipo){
+            const novoPokemon = {
+              id: db.pokemons.length +1,
+              nome,
+              nivel: parseInt(nivel),
+              tipo: tipo.split(' ')
+            }
+            db.pokemons.push(novoPokemon)
+            console.table(db.pokemons)
+          }
+          registroPokemon(nomePokemon,nivelPokemon,tipoPokemon)
           rl.close()
         });
       });
@@ -22,9 +32,11 @@ rl.question('O que você gostaria de fazer com seus pokemons? \n 1. Registrar \n
   } else {
     rl.question('Qual o ID do pokemon? \n', function(idPokemon) {
       rl.question('Quantos níveis quer adicionar? \n', function(niveisPokemon) {
-        console.log('ID:', idPokemon, 'Níveis:', niveisPokemon)
-        // Você pode remover esse console.log acima se quiser.
-        // Chame AQUI a função que irá receber os valores e treinar o pokemon.
+       // console.log('ID:', idPokemon, 'Níveis:', niveisPokemon)
+
+       
+        
+        
         rl.close()
       });
     });
