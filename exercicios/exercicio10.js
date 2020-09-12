8fc9a178bba9a4269176cf33b7874a2c48544270
Audry-prog <audryavalos@hotmail.com>
2020-09-12 20:09:06 -0300

    instaleei o npm re

--- exercicios/exercicio10.js
+++ exercicios/exercicio10.js
@@ -13,13 +13,20 @@ console.log ('Estou pensando num numero secreto entre 0 e 50. Tente advinhar!')
 let resposta = 0
     
 do {
-	let resposta = input.questionInt('Qual o numero secreto?');
+	let resposta = input.questionInt('Qual o numero secreto? ');
 	if (resposta < numSecreto) {
-		console.log('Errado! Seu palpite está muito baixo.Tente outra vez: ');
+		console.log('Errado! Seu palpite está muito baixo. Tente outra vez: ');
 	} else if (resposta > numSecreto) {
-		console.log('Errado! Seu palpite está muito alto.Tente outra vez: ');
+		console.log('Errado! Seu palpite está muito alto. Tente outra vez: ');
 	} else if (resposta === numSecreto) {
-		console.log('Parabéns você acertou o numero!');
+		console.log('Parabéns, você acertou o numero!');
 		break;
 	}
 } while (resposta != numSecreto);
+
+/* Saída:
+Estou pensando num numero secreto entre 0 e 50. Tente advinhar!
+Qual o numero secreto? 50
+Errado! Seu palpite está muito alto. Tente outra vez:
+Qual o numero secreto? 20
+Parabéns você acertou o numero!*/
--- projeto/pokemons.js
+++ projeto/pokemons.js
@@ -1,33 +1,77 @@
-const db = require('./database')
+const db = require('./database');
 const readline = require('readline');
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+	input: process.stdin,
+	output: process.stdout,
 });
 
-console.table(db.pokemons)
+console.table(db.pokemons);
 
-rl.question('O que você gostaria de fazer com seus pokemons? \n 1. Registrar \n 2. Treinar \n', function(instrucao) {
-  if (instrucao == 'registrar') {
-    rl.question('Qual o nome do pokemon? \n', function(nomePokemon) {
-      rl.question('Qual o nível do pokemon? \n', function(nivelPokemon) {
-        rl.question('Qual o tipo do pokemon? \n', function(tipoPokemon) {
-          console.log('Nome:', nomePokemon, 'Nível:', nivelPokemon, 'Tipo:', tipoPokemon)
-          // Você pode remover esse console.log acima se quiser.
-          // Chame AQUI a função que irá receber os valores e registrar o pokemon.
-          rl.close()
+function menu() {
+
+	rl.question('Bem-vindo ao Pokedex! O que você gostaria de fazer com seus Pokemons? \n 1. Registrar \n 2. Treinar \n 3. Sair \n', instrucao => {
+			if (instrucao == 'Registrar' || instrucao == 'registrar' || instrucao == 1) {
+				rl.question('\n Qual o nome do pokemon? \n', (nomePokemon) => {
+					rl.question('\n Qual o nível do pokemon? \n', (nivelPokemon) => {
+						rl.question('\n Qual o tipo do pokemon? \n', (tipoPokemon) => {
+              
+              const registrarPokemon = (nome, nivel, tipo) => {
+								const pokemon = {
+									id: db.pokemons.length + 1,
+									nome: nome,
+									nivel: parseInt(nivel),
+									tipo: tipo.split(', '),
+                };
+                
+								return db.pokemons.push(pokemon);
+							};
+
+							registrarPokemon(nomePokemon, nivelPokemon, tipoPokemon);
+							console.log(`\n ${nomePokemon} foi registrado na Pokedex. \n`);
+							console.table(db.pokemons);
+							rl.close();
+						});
+					});
         });
-      });
-    });
-  } else {
-    rl.question('Qual o ID do pokemon? \n', function(idPokemon) {
-      rl.question('Quantos níveis quer adicionar? \n', function(niveisPokemon) {
-        console.log('ID:', idPokemon, 'Níveis:', niveisPokemon)
-        // Você pode remover esse console.log acima se quiser.
-        // Chame AQUI a função que irá receber os valores e treinar o pokemon.
-        rl.close()
-      });
-    });
-  }
-  console.log(instrucao)
-});
+        
+			}else if (instrucao == 'Treinar' || instrucao == 'treinar' || instrucao == 2) {
+        question = () => {
+					rl.question('Qual o ID do Pokemon? \n', idPokemon => {
+						if (!db.pokemons[idPokemon - 1]) {
+							console.log('Pokemon não encontrado na Pokedex. Tente novamente.');
+							question();
+						}
+						rl.question('Quantos níveis quer adicionar? \n', niveisPokemon => {
+							const treinarPokemon = (id, niveis) => {
+								const novoNivel = db.pokemons[id - 1].nivel + parseInt(niveis);
+								if (novoNivel > 100) {
+									db.pokemons[id - 1].nivel = 100;
+								} else {
+									db.pokemons[id - 1].nivel = novoNivel;
+								}
+              };
+              
+							treinarPokemon (idPokemon, niveisPokemon)
+							console.log(`\n ${db.pokemons[idPokemon - 1].nome} subiu de nível na Pokedex. \n`);
+							const maiorNivel = db.pokemons.sort((a, b) => b.nivel - a.nivel);
+							console.table(maiorNivel);
+              rl.close();
+          
+						});
+					});
+        };
+        question();
+
+			} else if (instrucao == 'Sair' || instrucao == 'sair' || instrucao == 3) {
+				console.log('Você saiu da Pokedex.');
+				rl.close();
+			} else {
+				console.log('\n Instrução inválida. Escolha uma das 3 opções: Registrar, Treinar ou Sair. \n');
+        menu();
+			}
+		});
+};
+menu();
+
+
+
