e4317ee5b50c476a7a308e9cb7e45fa9f77e3919
eline-create <eline.geo@gmail.com>
2020-09-12 14:38:49 -0300

    Segunda parte do projeto começando a ser resolvida. Falta fazer a soma dentro da tabela

--- Entrega/projeto.js
+++ Entrega/projeto.js
@@ -1,11 +1,12 @@
 const db = require('../projeto/database')
 const input = require('readline-sync')
+const { pokemons } = require('../projeto/database')
 
 console.log('== versão com readline-sync ==')
 console.table(db.pokemons)
 
 const instrucao = input.question('O que você gostaria de fazer com seus pokemons? \n 1. Registrar \n 2. Treinar \n')
-if (instrucao == 'registrar' || instrucao == "Registrar" || instrucao == '1') {
+if (instrucao == 'registrar' || instrucao == 'Registrar' || instrucao == '1') {
     const nomePokemon = input.question('Qual o nome do pokemon? \n')
     const nivelPokemon = input.question('Qual o nível do pokemon? \n')
     const tipoPokemon = input.question('Qual o tipo do pokemon? Se mais de um tipo, separe por vírgulas \n')
@@ -26,23 +27,26 @@ if (instrucao == 'registrar' || instrucao == "Registrar" || instrucao == '1') {
   // Chame AQUI a função que irá receber os valores e registrar o pokemon. 
     regPokemon(nomePokemon, nivelPokemon, tipoPokemon)
     console.table(db.pokemons)
-
-
     
 }
-else {
-    (instrucao == 'treinar' || instrucao == "Treinar" || instrucao == 2)
+else if (instrucao == 'treinar' || instrucao == 'Treinar' || instrucao == '2') {
     const idPokemon = input.question('Qual o ID do pokemon? \n')
     const niveisPokemon = input.question('Quantos níveis quer adicionar? \n')
     console.log('ID:', idPokemon, 'Níveis:', niveisPokemon)
     // Você pode remover esse console.log acima se quiser.
 
 
+    function trainPokemon(element, level){
+        if (idPokemon == element.id){
+            console.log = element.nivel + parseInt(niveisPokemon)
+        }
+        
+    }   
 
+            // Chame AQUI a função que irá receber os valores e treinar o pokemon.
+    pokemons.forEach(trainPokemon)
+    trainPokemon(idPokemon, niveisPokemon)
+    console.table(db.pokemons)
 
-
-    // Chame AQUI a função que irá receber os valores e treinar o pokemon.
 }
-
-
 
