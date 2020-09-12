773565e2aba8f979fe44f6ac3d9d6a89a3f3d5e7
mariana.vns <mariana.vns@gmail.com>
2020-09-12 16:01:05 -0300

    Resolve o exercício 7

--- exercicios/exercicio7.js
+++ exercicios/exercicio7.js
@@ -1,1 +1,14 @@
 const valores = [2, 34, 56, 3, 9, 6, 55, 106];
+
+function somaImpar (arr) {
+	let acumulador = 0
+	for (let i = 0; i < arr.length; i++){
+		if (arr[i] % 2 !== 0){
+			acumulador += arr[i]
+		}
+	}
+	return console.log(`A soma dos números ímpares nesta lista é ${acumulador}.`)
+}
+			
+	
+	somaImpar ([1, 3, 1000, 51, 10, 200003])
