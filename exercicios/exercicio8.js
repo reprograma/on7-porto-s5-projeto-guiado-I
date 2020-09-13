6daff6ce62c94773420936aef473e4387db07d76
mariana.vns <mariana.vns@gmail.com>
2020-09-12 22:04:42 -0300

    Responde o exercício 8

--- exercicios/exercicio8.js
+++ exercicios/exercicio8.js
@@ -1,1 +1,14 @@
 const nomes = ['Anna', 'Andressa', 'Luciana', 'Bruna', 'Amanda'];
+
+function filtraA (arr) {
+    const palavrasA = []
+       for (let i = 0; i < arr.length; i++){
+         if (arr[i][0] == "a" || arr[i][0] == "á" || arr[i][0] == "A" || arr[i][0] == "Á") {
+          palavrasA.push(arr[i])
+         
+       }
+       }
+    console.log(palavrasA)
+    }
+
+filtraA(nomes)
