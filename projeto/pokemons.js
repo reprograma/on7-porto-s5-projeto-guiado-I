7754bf9eb058b24ac045359c97d27ab3462ad3bc
jessica-MBS <jessica.aparecida@gmail.com>
2020-09-12 21:34:12 -0300

    projeto - concluido busca por ID válido

--- projeto/pokemons.js
+++ projeto/pokemons.js
@@ -8,7 +8,7 @@ const rl = readline.createInterface({
 console.table(db.pokemons)
 
 rl.question('O que você gostaria de fazer com seus pokemons? \n 1. Registrar \n 2. Treinar \n', function(instrucao) {
-  if (instrucao == 'registrar') {
+  if (instrucao == 'registrar' || instrucao == 'Registrar' || instrucao == 1) {
     rl.question('Qual o nome do pokemon? \n', function(nomePokemon) {
       rl.question('Qual o nível do pokemon? \n', function(nivelPokemon) {
         rl.question('Qual o tipo do pokemon? \n', function(tipoPokemon) {
@@ -22,6 +22,7 @@ rl.question('O que você gostaria de fazer com seus pokemons? \n 1. Registrar \n
               tipo: tipo.split(' ')
             }
             db.pokemons.push(novoPokemon)
+            console.log(nome + ' foi resgistrado com sucesso')
             console.table(db.pokemons)
           }
           registroPokemon(nomePokemon,nivelPokemon,tipoPokemon)
@@ -29,14 +30,20 @@ rl.question('O que você gostaria de fazer com seus pokemons? \n 1. Registrar \n
         });
       });
     });
-  } else {
+  } else if (instrucao == 'treinar' || instrucao == 'Treinar' || instrucao == 2) {
     rl.question('Qual o ID do pokemon? \n', function(idPokemon) {
+      const buscaPokemon = db.pokemons.filter( pokemon => pokemon.id == idPokemon)
+      if (buscaPokemon.length === 0) {
+        console.error('Pokemon não encontrado')     
+      } else 
       rl.question('Quantos níveis quer adicionar? \n', function(niveisPokemon) {
-       // console.log('ID:', idPokemon, 'Níveis:', niveisPokemon)
+       //console.log('ID:', idPokemon, 'Níveis:', niveisPokemon)
+       const treinandoPokemon = db.pokemons.
 
-       
+
         
-        
+       console.log(nome + ' foi treinado com sucesso')
+       console.table(db.pokemons)
         rl.close()
       });
     });
