1853a70c8099ec992150850e9b14840de83b7e63
Tereza25 <rh.cris.ead@gmail.com>
2020-12-05 11:20:28 -0300

    modificando

--- projeto/pokemons.js
+++ projeto/pokemons.js
@@ -7,19 +7,32 @@ const rl = readline.createInterface({
 
 console.table(db.pokemons)
 
-rl.question('O que você gostaria de fazer com seus pokemons? \n 1. Registrar \n 2. Treinar \n', function(instrucao) {
-  if (instrucao == 'registrar') {
-    rl.question('Qual o nome do pokemon? \n', function(nomePokemon) {
-      rl.question('Qual o nível do pokemon? \n', function(nivelPokemon) {
-        rl.question('Qual o tipo do pokemon? \n', function(tipoPokemon) {
-          console.log('Nome:', nomePokemon, 'Nível:', nivelPokemon, 'Tipo:', tipoPokemon)
-          // Você pode remover esse console.log acima se quiser.
-          // Chame AQUI a função que irá receber os valores e registrar o pokemon.
-          rl.close()
+let menu = () => {
+  
+  rl.question('O que você gostaria de fazer? \n 1. Registrar pokémon \n 2. Treinar pokémon \n 3. Sair da pokédex \n', option => {
+    if (option == 1 || option == 'Registrar' || option == 'registrar') {
+        rl.question('\n Qual o nome do pokémon? \n', namePokemon => {
+            rl.question('\n Qual o nível do pokémon? \n', levelPokemon => {
+                rl.question('\n Qual(is) o(s) tipo(s) do pokémon? \n', typePokemon => {
+
+                    const registerPokemon = (name, level, type) => {
+                        const pokemon = {
+                            id: db.pokemons.length + 1,
+                            name,
+                            level: parseInt(level),
+                            type: type.split(', ')
+                        };
+                        return db.pokemons.push(pokemon);
+                    };
+
+                    registerPokemon(namePokemon, levelPokemon, typePokemon);
+                    console.log(`\n O pokémon ${namePokemon} foi registrado com sucesso.\n`)
+                    console.table(db.pokemons)
+                    rl.close();
+                });
+            });
         });
-      });
-    });
-  } else {
+      } else {
     rl.question('Qual o ID do pokemon? \n', function(idPokemon) {
       rl.question('Quantos níveis quer adicionar? \n', function(niveisPokemon) {
         console.log('ID:', idPokemon, 'Níveis:', niveisPokemon)
